9e03e6026f4f740fdb5b3f6706510e5af58463a6
Charan K <charankulal0241@gmail.com>
2025-03-22 19:50:17 +0530

    Create Student Feature

--- lib/pages/admin/students/add_student_page.dart
+++ lib/pages/admin/students/add_student_page.dart
@@ -1,3 +1,4 @@
+import 'package:bus_tracking_app/services/admin/students.dart';
 import 'package:flutter/material.dart';
 
 class AddStudentPage extends StatefulWidget {
@@ -8,8 +9,94 @@ class AddStudentPage extends StatefulWidget {
 class _AddStudentPageState extends State<AddStudentPage> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   String? _selectedRoute;
-  final List<String> _routes = ["Route A", "Route B", "Route C", "Route D"];
+  List<Map<String, dynamic>> _routesList =[];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchRoutes();
+  }
+
+  void fetchRoutes() async {
+    _routesList = await StudentsDatabaseServices().getAllRoutesIDName();
+    setState(() {});
+  }
+
+  void _addStudent() async {
+    final RegExp phoneNumRegEx = RegExp(r'^[6-9]\d{9}$');
+    final RegExp nameRegEx = RegExp(r"^[A-Za-z]+(?:[-' ][A-Za-z]+)*$");
+    final RegExp passwordRegEx = RegExp(r"^(?=.*[A-Za-z])(?=.*\d).{8,}$");
+
+    if (_nameController.text.isEmpty ||
+        _phoneController.text.isEmpty ||
+        _passwordController.text.isEmpty ||
+        _selectedRoute == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Please fill all fields"),
+          duration: Duration(seconds: 1),
+        ),
+      );
+      return;
+    }
+    if (!nameRegEx.hasMatch(_nameController.text)) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Please enter valid name"),
+          duration: Duration(seconds: 1),
+        ),
+      );
+      return;
+    }
+    if (!phoneNumRegEx.hasMatch(_phoneController.text)) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Please enter valid phone number"),
+          duration: Duration(seconds: 1),
+        ),
+      );
+      return;
+    }
+    if (!passwordRegEx.hasMatch(_passwordController.text)) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Please enter valid password"),
+          duration: Duration(seconds: 1),
+        ),
+      );
+      return;
+    }
+
+    Map<String, dynamic> studentInfo = {
+      "name": _nameController.text,
+      "phone": _phoneController.text,
+      "password":_passwordController.text,
+      "route":_selectedRoute
+    };
+    await StudentsDatabaseServices()
+        .addStudent(studentInfo)
+        .then((value) {
+      Navigator.pop(context);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Student Details Added Successfully"),
+          duration: Duration(seconds: 1),
+        ),
+      );
+    })
+        .catchError((error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            "Failed to add student details: ${error.toString()}",
+          ),
+          duration: Duration(seconds: 1),
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,35 +133,38 @@ class _AddStudentPageState extends State<AddStudentPage> {
                 border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
               ),
             ),
+
+            Text("Password", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+            SizedBox(height: 6),
+            TextField(
+              controller: _phoneController,
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                hintText: "Enter phone number",
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+              ),
+            ),
             SizedBox(height: 16),
 
             // Route Selection (Dropdown)
             Text("Select Route", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
             SizedBox(height: 6),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 12),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: DropdownButtonHideUnderline(
-                child: DropdownButton<String>(
-                  value: _selectedRoute,
-                  hint: Text("Choose a route"),
-                  isExpanded: true,
-                  onChanged: (value) {
-                    setState(() {
-                      _selectedRoute = value;
-                    });
-                  },
-                  items: _routes.map((route) {
-                    return DropdownMenuItem(
-                      value: route,
-                      child: Text(route),
-                    );
-                  }).toList(),
+            DropdownButtonFormField<String>(
+              value: _selectedRoute??"qwerty",
+              items: [
+                DropdownMenuItem<String>(
+                  value: 'qwerty',
+                  child: Text("Select Route", style: TextStyle(color: Colors.black)),
                 ),
-              ),
+                ..._routesList.map((route) {
+                  return DropdownMenuItem<String>(
+                    value: route["routeId"],
+                    child: Text(route["route_name"]),
+                  );
+                }).toList(),
+              ],
+              onChanged: (value) => setState(() => _selectedRoute = value),
+              decoration: InputDecoration(border: OutlineInputBorder()),
             ),
             SizedBox(height: 24),
 
@@ -89,7 +179,7 @@ class _AddStudentPageState extends State<AddStudentPage> {
                     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
                   ),
                   onPressed: () {
-                    // Save Student Logic Here
+                    _addStudent();
                   },
                   child: Text("Save", style: TextStyle(color: Colors.white, fontSize: 16)),
                 ),
---
+++ lib/services/admin/students.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class StudentsDatabaseServices {
+  final CollectionReference studentCollection = FirebaseFirestore.instance
+      .collection("student");
+
+  Future<void> addStudent(Map<String, dynamic> studentInfo) async {
+    DocumentReference docRef = studentCollection.doc();
+    String studentId = docRef.id;
+
+    studentInfo['studentId'] = studentId;
+
+    await docRef.set(studentInfo);
+  }
+
+  Future<List<Map<String, dynamic>>> getAllRoutesIDName() async {
+    QuerySnapshot querySnapshot =
+    await FirebaseFirestore.instance.collection("route").get();
+
+    return querySnapshot.docs.map((doc) {
+      return {"routeId": doc.id, "route_name": doc["route_name"]};
+    }).toList();
+  }
+}
